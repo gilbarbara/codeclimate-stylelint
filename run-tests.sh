541bd1bd2cd67bd452507ce9e888e2d67b325be1
Gil Barbara <gilbarbara@gmail.com>
2019-07-12 19:05:15 -0300

    Setup docker to run the tests on travis CI

--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,13 @@ for d in test/*/ ; do
     echo ""
     echo "▶ Running $d ..."
     cd ${SOURCE}/${d}
-    codeclimate analyze --dev 1>/dev/null
+    docker run \
+      --interactive --tty --rm \
+      --env CODECLIMATE_CODE="$PWD" \
+      --volume "$PWD":/code \
+      --volume /var/run/docker.sock:/var/run/docker.sock \
+      --volume /tmp/cc:/tmp/cc \
+      codeclimate/codeclimate analyze --dev 1>/dev/null
     if [[ "$?" -ne 0 ]]; then
       break
     fi
